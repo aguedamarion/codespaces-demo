ef88b85982596a3cbdcba779df0ebc507a8692a6
aguedamarion <81313892+aguedamarion@users.noreply.github.com>
2022-11-20 18:55:48 -0300

    Update if-else.sh

--- if-else.sh
+++ if-else.sh
@@ -3,9 +3,9 @@
 echo "What food do you choose?"
 read FOOD
 
-if [ "$FOOD" = "Apple" ]: then
+if [ "$FOOD" = "Apple" ]; then
   echo "Eat yogurt with your apple."
- elif [ "$FOOD" = "Milk" ]: then
+ elif [ "$FOOD" = "Milk" ]; then
   echo "Eat cereal with your milk."
  else
   echo "Eat your ${FOOD} by itself."
